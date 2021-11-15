c756fe2075bb04d6d63a8c399f049a81110a2d68
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2021-11-15 10:28:53 +0100

    Grid create

---
+++ src/fr/uvsq/iut-velizy-rambouillet/game/librairies/Point.py
@@ -0,0 +1,101 @@
+#Author : Chartier Hugo
+from math import *
+
+class Point:
+    """ Crée un point de Coordonnée X Y
+        X -> int
+        Y -> int """
+    
+    def __init__(self, x : int, y: int):
+        #constructeur de Point attrbut à x et y les valeur d'appel
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        #retourne  la coord x
+        return self._x
+
+    @property
+    def y(self):
+        #retourne  la coord y
+       return self._y
+
+    @x.setter
+    def set_x(self, pX):
+        #Defini la coord x selon pX
+        self._x = pX
+
+    @y.setter
+    def set_y(self, pY):
+        #Defini la coord y selon pY
+        self._y = pY
+    
+
+    @property
+    def get(self):
+        #Retourne le Point -> (x, y)
+
+        return (self.x, self.y)
+
+
+    @staticmethod
+    def vectorBetweenPoint(pA, pB) -> tuple:
+        """Calcul Le vecteur entre deux points
+            pA -> Point
+            pB -> Point """
+
+        lVector = Point((pB.x - pA.y), (pB.y - pA.y))
+        return lVector.get
+
+    @staticmethod
+    def getDistanceBetweenPoint(pA , pB) -> float:
+        """Calcul la distance entre deux points
+            pA -> Point
+            pB -> Point """
+
+        lDistance = 0
+
+        lDistance = sqrt(((pA.x - pB.x) * (pA.x - pB.x)) + ((pA.y - pB.y) * (pA.y - pB.y)))
+        return lDistance
+
+    @staticmethod
+    def cartesianToPolar(pA) -> tuple:
+        """Transforme des coordonées Cartésienne en coordonées Polaire"""
+        RADTODEG = 180/pi
+
+        lR = 0
+        lAngle = 0
+
+
+        lR = floor(sqrt((pA.x * pA.x)+ (pA.y * pA.y)))
+        lAngle = tanh((pA.y/pA.x))
+
+        lAngle *= RADTODEG
+
+        return (lR, ceil(lAngle))
+
+
+    @staticmethod
+    def polarToCartesian(pA) -> tuple:
+        """Transforme des coordonées Polaire en coordonées Cartésienne"""
+        lX = 0
+        lY = 0
+
+        DEGTORAD = pi/180
+
+        lAngle = pA.y * DEGTORAD
+
+        lX = ceil(pA.x * cos(lAngle))
+        lY = ceil(pA.x * sin(lAngle))
+
+        return (lX, lY)
+
+    @staticmethod
+    def normalize(pA) -> tuple:
+        """Normalise le vecteur """
+        lNorme = sqrt((pA.x * pA.x) + (pA.y * pA.y))
+
+        lNormalizedVector = (pA.x / lNorme, pA.y / lNorme)
+
+        return lNormalizedVector
--- src/fr/uvsq/iut-velizy-rambouillet/game/main.py
+++ src/fr/uvsq/iut-velizy-rambouillet/game/main.py
@@ -1,19 +1,46 @@
 #coding: utf-8
 from librairies.tkiteasy import *
+from librairies.Point import *
 
+
+cellSize = 30
+stageHeight = 900
+stageWidth = 930
+
+def drawGrid(pObjGraph, pCellSize, pOriginX, pOriginY, pEndY, pEndX):
+    lOriginX = pOriginX
+    lOriginY = pOriginY
+
+    lStartLigneCol = 0
+    lStartLigneRaw = 0
+
+    lNcellCol = int(stageWidth / pCellSize)
+    lNCellRaw = int(stageHeight / pCellSize)
+
+    for i in range(lNCellRaw):
+        
+        lRaw = pObjGraph.dessinerLigne(0, lStartLigneRaw, pEndX, lStartLigneRaw, "white")
+        pObjGraph.update()
+        lStartLigneRaw += pCellSize
+    
+    for j in range(lNcellCol):
+
+        lCol = pObjGraph.dessinerLigne(lStartLigneCol, 0, lStartLigneCol , pEndY, "white")
+        pObjGraph.update()
+        
+        lStartLigneCol += pCellSize
+        
 
 # ouverture de fenêtre
-g = ouvrirFenetre(800,600)
+g = ouvrirFenetre(stageWidth, stageHeight)
 
 # afficher image
-pacman = g.dessinerCercle(250, 250, 150, "white")
+drawGrid(g, cellSize, 0 , 0, stageHeight , stageWidth)
 
 while(True):
+
     test = g.recupererClic()
     g.update()
 
     if(test):
         g.fermerFenetre()
-
-
-
