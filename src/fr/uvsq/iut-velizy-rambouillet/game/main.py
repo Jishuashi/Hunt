3e5051208c2b570bc376b788623fa1013b6291ec
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2021-11-15 11:37:04 +0100

    Spawn Prey

--- src/fr/uvsq/iut-velizy-rambouillet/game/main.py
+++ src/fr/uvsq/iut-velizy-rambouillet/game/main.py
@@ -1,11 +1,22 @@
 #coding: utf-8
 from librairies.tkiteasy import *
 from librairies.Point import *
+from random import *
 
 
 cellSize = 30
 stageHeight = 900
-stageWidth = 930
+stageWidth = 900
+grid = [ ]
+RAYON = 10
+
+def drawProie():
+    spawnX = randrange(15, stageHeight, cellSize)
+    spawnY = randrange(15, stageHeight, cellSize)
+    Ligne = (spawnX - 15)/30
+    Col = (spawnY - 15)/30
+    g.dessinerDisque(spawnX, spawnY, RAYON, "green")
+    g.update()
 
 def drawGrid(pObjGraph, pCellSize, pOriginX, pOriginY, pEndY, pEndX):
     lOriginX = pOriginX
@@ -16,19 +27,26 @@ def drawGrid(pObjGraph, pCellSize, pOriginX, pOriginY, pEndY, pEndX):
 
     lNcellCol = int(stageWidth / pCellSize)
     lNCellRaw = int(stageHeight / pCellSize)
-
-    for i in range(lNCellRaw):
-        
-        lRaw = pObjGraph.dessinerLigne(0, lStartLigneRaw, pEndX, lStartLigneRaw, "white")
-        pObjGraph.update()
-        lStartLigneRaw += pCellSize
-    
     for j in range(lNcellCol):
 
         lCol = pObjGraph.dessinerLigne(lStartLigneCol, 0, lStartLigneCol , pEndY, "white")
         pObjGraph.update()
         
         lStartLigneCol += pCellSize
+        grid.append({})
+        
+
+
+    for i in range(lNCellRaw):
+        
+        lRaw = pObjGraph.dessinerLigne(0, lStartLigneRaw, pEndX, lStartLigneRaw, "white")
+        pObjGraph.update()
+        lStartLigneRaw += pCellSize
+        
+        for m in range(lNCellRaw):
+            grid[i][str(m)] = [False , None]
+    
+    
         
 
 # ouverture de fenêtre
@@ -36,6 +54,9 @@ g = ouvrirFenetre(stageWidth, stageHeight)
 
 # afficher image
 drawGrid(g, cellSize, 0 , 0, stageHeight , stageWidth)
+
+print(grid)
+drawProie()
 
 while(True):
 
